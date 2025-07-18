050203a1267759d7b092ce502b98088286b1276e
emmz3230 <emmz3230@gmail.com>
2025-07-18 10:57:50 +0100

    code structure for bigger-project practice

--- code-structuring-for-big-project.js
+++ code-structuring-for-big-project.js
@@ -1,213 +1,256 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import GUI from "lil-gui";
+class Robot {
+  constructor(name, legs) {
+    this.name = name;
+    this.legs = legs;
 
-const gltfLoader = new GLTFLoader();
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-const textureLoader = new THREE.TextureLoader();
-
-/**
- * Base
- */
-// Debug
-const gui = new GUI();
-const debugObject = {};
-
-// Canvas
-const canvas = document.querySelector("canvas.webgl");
-
-// Scene
-const scene = new THREE.Scene();
-
-const updateAllMaterials = () => {
-  scene.traverse((child) => {
-    if (child.isMesh && child.material.isMeshStandardMaterial) {
-      child.material.envMapIntensity = global.envMapIntensity;
-
-      child.material.envMapIntensity = debugObject.envMapIntensity;
-      child.material.needsUpdate = true;
-      child.castShadow = true;
-      child.receiveShadow = true;
-    }
-  });
-};
-/**
- * Environment map
- */
-const environmentMap = cubeTextureLoader.load([
-  "/textures/environmentMap/px.jpg",
-  "/textures/environmentMap/nx.jpg",
-  "/textures/environmentMap/py.jpg",
-  "/textures/environmentMap/ny.jpg",
-  "/textures/environmentMap/pz.jpg",
-  "/textures/environmentMap/nz.jpg",
-]);
-
-environmentMap.colorSpace = THREE.SRGBColorSpace;
-
-// scene.background = environmentMap
-scene.environment = environmentMap;
-
-debugObject.envMapIntensity = 0.4;
-
-gui
-  .add(debugObject, "envMapIntensity")
-  .min(0)
-  .max(4)
-  .step(0.001)
-  .onChange(updateAllMaterials);
-
-let foxMixer = null;
-
-gltfLoader.load("/models/Fox/glTF/Fox.gltf", (gltf) => {
-  // Model
-  gltf.scene.scale.set(0.02, 0.02, 0.02);
-  scene.add(gltf.scene);
-
-  // Animation
-  foxMixer = new THREE.AnimationMixer(gltf.scene);
-  const foxAction = foxMixer.clipAction(gltf.animations[0]);
-  foxAction.play();
-
-  // Update materials
-  updateAllMaterials();
-});
-
-const floorColorTexture = textureLoader.load(
-  "textures/dirt/color.jpg"
-);
-floorColorTexture.colorSpace = THREE.SRGBColorSpace;
-floorColorTexture.repeat.set(1.5, 1.5);
-floorColorTexture.wrapS = THREE.RepeatWrapping;
-floorColorTexture.wrapT = THREE.RepeatWrapping;
-
-const floorNormalTexture = textureLoader.load(
-  "textures/dirt/normal.jpg"
-);
-floorNormalTexture.repeat.set(1.5, 1.5);
-floorNormalTexture.wrapS = THREE.RepeatWrapping;
-floorNormalTexture.wrapT = THREE.RepeatWrapping;
-
-const floorGeometry = new THREE.CircleGeometry(5, 64);
-const floorMaterial = new THREE.MeshStandardMaterial({
-  map: floorColorTexture,
-  normalMap: floorNormalTexture,
-});
-const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-floor.rotation.x = -Math.PI * 0.5;
-scene.add(floor);
-
-const directionalLight = new THREE.DirectionalLight("#ffffff", 1);
-directionalLight.castShadow = true;
-directionalLight.shadow.camera.far = 15;
-directionalLight.shadow.mapSize.set(1024, 1024);
-directionalLight.shadow.normalBias = 0.05;
-directionalLight.position.set(3.5, 2, -1.25);
-scene.add(directionalLight);
-
-gui
-  .add(directionalLight, "intensity")
-  .min(0)
-  .max(10)
-  .step(0.001)
-  .name("lightIntensity");
-gui
-  .add(directionalLight.position, "x")
-  .min(-5)
-  .max(5)
-  .step(0.001)
-  .name("lightX");
-gui
-  .add(directionalLight.position, "y")
-  .min(-5)
-  .max(5)
-  .step(0.001)
-  .name("lightY");
-gui
-  .add(directionalLight.position, "z")
-  .min(-5)
-  .max(5)
-  .step(0.001)
-  .name("lightZ");
-
-/**
- * Sizes
- */
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
-
-window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(
-  35,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-camera.position.set(6, 5, 4);
-scene.add(camera);
-
-// Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-  antialias: true,
-});
-renderer.toneMapping = THREE.CineonToneMapping;
-renderer.toneMappingExposure = 1.75;
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-renderer.setClearColor("#211d20");
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-/**
- * Animate
- */
-const clock = new THREE.Clock();
-let previousTime = 0;
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-  const deltaTime = elapsedTime - previousTime;
-  previousTime = elapsedTime;
-
-  // Update controls
-  controls.update();
-
-  // Fox animation
-  if (foxMixer) {
-    foxMixer.update(deltaTime);
+    console.log(`I am ${this.name}. Thank you creator.`);
+    this.sayHi();
   }
 
-  // Render
-  renderer.render(scene, camera);
+  sayHi() {
+    console.log(`Hello! My name is ${this.name}`);
+  }
+}
 
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
+class FlyingRobot extends Robot {
+  constructor(name, legs) {
+    super(name, legs);
+    super.sayHi();
+    console.log(`I am a flying robot `);
+  }
+  sayHi() {
+    console.log(
+      `Hello! My name is ${this.name} and i am a flying robot`
+    );
+  }
+  takeOff() {
+    console.log(`Have a good flight ${this.name}`);
+  }
 
-tick();
+  land() {
+    console.log(`welcome back ${this.name}`);
+  }
+}
+
+const wallE = new Robot("Wall-E", 0);
+const ultron = new Robot("Ultron", 2);
+const astroBoy = new Robot("Astro-Boy", 2);
+
+ultron.sayHi("Ultron");
+astroBoy.sayHi("Astro-Boy");
+
+// console.log(robot);
+
+// import * as THREE from "three";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+// import GUI from "lil-gui";
+
+// const gltfLoader = new GLTFLoader();
+// const cubeTextureLoader = new THREE.CubeTextureLoader();
+// const textureLoader = new THREE.TextureLoader();
+
+// /**
+//  * Base
+//  */
+// // Debug
+// const gui = new GUI();
+// const debugObject = {};
+
+// // Canvas
+// const canvas = document.querySelector("canvas.webgl");
+
+// // Scene
+// const scene = new THREE.Scene();
+
+// const updateAllMaterials = () => {
+//   scene.traverse((child) => {
+//     if (child.isMesh && child.material.isMeshStandardMaterial) {
+//       child.material.envMapIntensity = global.envMapIntensity;
+
+//       child.material.envMapIntensity = debugObject.envMapIntensity;
+//       child.material.needsUpdate = true;
+//       child.castShadow = true;
+//       child.receiveShadow = true;
+//     }
+//   });
+// };
+// /**
+//  * Environment map
+//  */
+// const environmentMap = cubeTextureLoader.load([
+//   "/textures/environmentMap/px.jpg",
+//   "/textures/environmentMap/nx.jpg",
+//   "/textures/environmentMap/py.jpg",
+//   "/textures/environmentMap/ny.jpg",
+//   "/textures/environmentMap/pz.jpg",
+//   "/textures/environmentMap/nz.jpg",
+// ]);
+
+// environmentMap.colorSpace = THREE.SRGBColorSpace;
+
+// // scene.background = environmentMap
+// scene.environment = environmentMap;
+
+// debugObject.envMapIntensity = 0.4;
+
+// gui
+//   .add(debugObject, "envMapIntensity")
+//   .min(0)
+//   .max(4)
+//   .step(0.001)
+//   .onChange(updateAllMaterials);
+
+// let foxMixer = null;
+
+// gltfLoader.load("/models/Fox/glTF/Fox.gltf", (gltf) => {
+//   // Model
+//   gltf.scene.scale.set(0.02, 0.02, 0.02);
+//   scene.add(gltf.scene);
+
+//   // Animation
+//   foxMixer = new THREE.AnimationMixer(gltf.scene);
+//   const foxAction = foxMixer.clipAction(gltf.animations[0]);
+//   foxAction.play();
+
+//   // Update materials
+//   updateAllMaterials();
+// });
+
+// const floorColorTexture = textureLoader.load(
+//   "textures/dirt/color.jpg"
+// );
+// floorColorTexture.colorSpace = THREE.SRGBColorSpace;
+// floorColorTexture.repeat.set(1.5, 1.5);
+// floorColorTexture.wrapS = THREE.RepeatWrapping;
+// floorColorTexture.wrapT = THREE.RepeatWrapping;
+
+// const floorNormalTexture = textureLoader.load(
+//   "textures/dirt/normal.jpg"
+// );
+// floorNormalTexture.repeat.set(1.5, 1.5);
+// floorNormalTexture.wrapS = THREE.RepeatWrapping;
+// floorNormalTexture.wrapT = THREE.RepeatWrapping;
+
+// const floorGeometry = new THREE.CircleGeometry(5, 64);
+// const floorMaterial = new THREE.MeshStandardMaterial({
+//   map: floorColorTexture,
+//   normalMap: floorNormalTexture,
+// });
+// const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+// floor.rotation.x = -Math.PI * 0.5;
+// scene.add(floor);
+
+// const directionalLight = new THREE.DirectionalLight("#ffffff", 1);
+// directionalLight.castShadow = true;
+// directionalLight.shadow.camera.far = 15;
+// directionalLight.shadow.mapSize.set(1024, 1024);
+// directionalLight.shadow.normalBias = 0.05;
+// directionalLight.position.set(3.5, 2, -1.25);
+// scene.add(directionalLight);
+
+// gui
+//   .add(directionalLight, "intensity")
+//   .min(0)
+//   .max(10)
+//   .step(0.001)
+//   .name("lightIntensity");
+// gui
+//   .add(directionalLight.position, "x")
+//   .min(-5)
+//   .max(5)
+//   .step(0.001)
+//   .name("lightX");
+// gui
+//   .add(directionalLight.position, "y")
+//   .min(-5)
+//   .max(5)
+//   .step(0.001)
+//   .name("lightY");
+// gui
+//   .add(directionalLight.position, "z")
+//   .min(-5)
+//   .max(5)
+//   .step(0.001)
+//   .name("lightZ");
+
+// /**
+//  * Sizes
+//  */
+// const sizes = {
+//   width: window.innerWidth,
+//   height: window.innerHeight,
+// };
+
+// window.addEventListener("resize", () => {
+//   // Update sizes
+//   sizes.width = window.innerWidth;
+//   sizes.height = window.innerHeight;
+
+//   // Update camera
+//   camera.aspect = sizes.width / sizes.height;
+//   camera.updateProjectionMatrix();
+
+//   // Update renderer
+//   renderer.setSize(sizes.width, sizes.height);
+//   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+// });
+
+// /**
+//  * Camera
+//  */
+// // Base camera
+// const camera = new THREE.PerspectiveCamera(
+//   35,
+//   sizes.width / sizes.height,
+//   0.1,
+//   100
+// );
+// camera.position.set(6, 5, 4);
+// scene.add(camera);
+
+// // Controls
+// const controls = new OrbitControls(camera, canvas);
+// controls.enableDamping = true;
+
+// /**
+//  * Renderer
+//  */
+// const renderer = new THREE.WebGLRenderer({
+//   canvas: canvas,
+//   antialias: true,
+// });
+// renderer.toneMapping = THREE.CineonToneMapping;
+// renderer.toneMappingExposure = 1.75;
+// renderer.shadowMap.enabled = true;
+// renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+// renderer.setClearColor("#211d20");
+// renderer.setSize(sizes.width, sizes.height);
+// renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// /**
+//  * Animate
+//  */
+// const clock = new THREE.Clock();
+// let previousTime = 0;
+// const tick = () => {
+//   const elapsedTime = clock.getElapsedTime();
+//   const deltaTime = elapsedTime - previousTime;
+//   previousTime = elapsedTime;
+
+//   // Update controls
+//   controls.update();
+
+//   // Fox animation
+//   if (foxMixer) {
+//     foxMixer.update(deltaTime);
+//   }
+
+//   // Render
+//   renderer.render(scene, camera);
+
+//   // Call tick again on the next frame
+//   window.requestAnimationFrame(tick);
+// };
+
+// tick();
---
+++ test.js
@@ -0,0 +1,9 @@
+const anotherThing = () => {
+  hello: "modules are working";
+};
+
+const oneThing = () => {
+  console.log("hi");
+};
+
+export { oneThing, anotherThing };
