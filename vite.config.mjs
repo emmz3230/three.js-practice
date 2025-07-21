34f85836e5c54fc2df2ef33c0db29a2b4179508f
emmz3230 <emmz3230@gmail.com>
2025-07-21 20:03:25 +0100

    add new code to the config(vite)

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,21 @@
 import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
 
+const isCodeSandbox = !!process.env.CODESANDBOX_HOST;
+
 export default defineConfig({
+  root: "src/",
+  publicDir: "../static/",
+  base: "./",
+  server: {
+    host: true,
+    open: !isCodeSandbox,
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+
   plugins: [glsl()],
 });
