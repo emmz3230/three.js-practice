b7e4aa0d8fe8889ce11b8d5658f749327ff1e537
emmz3230 <emmz3230@gmail.com>
2025-08-22 04:18:08 +0100

    starter file for hologrphic shader

---
+++ hologram-shader.js
@@ -0,0 +1,130 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+import GUI from "lil-gui";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+import vertexShader from "./shaders/holographic/vertex.glsl";
+import fragmentShader from "./shaders/holographic/fragment.glsl";
+
+// base
+
+// debug
+const gui = new GUI();
+
+// canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// scene
+const scene = new THREE.Scene();
+
+// loaders
+const gltfLoader = new GLTFLoader();
+
+// sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(7, 7, 7);
+scene.add(camera);
+
+// controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+// renderer
+const rendererParameters = {};
+rendererParameters.clearColor = "#1d1f2a";
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setClearColor(rendererParameters.clearColor);
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+gui.addColor(rendererParameters, "clearColor").onChange(() => {
+  renderer.setClearColor(rendererParameters.clearColor);
+});
+
+// material
+const material = new THREE.ShaderMaterial({
+  vertexShader: vertexShader,
+  fragmentShader: fragmentShader,
+});
+
+// objects
+// torus knot
+const torusKnot = new THREE.Mesh(
+  new THREE.TorusKnotGeometry(0.6, 0.25, 128, 32),
+  material
+);
+torusKnot.position.x = 3;
+scene.add(torusKnot);
+
+// sphere
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(), material);
+sphere.position.x = -3;
+scene.add(sphere);
+
+// suzuanne
+let suzuanne = null;
+gltfLoader.load("./suzanne.glb", (gltf) => {
+  suzuanne = gltf.scene;
+  suzuanne.traverse((child) => {
+    if (child.isMesh) {
+      child.material = material;
+    }
+  });
+  scene.add(suzuanne);
+});
+
+// animate
+const clock = new THREE.Clock();
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // rotate objects
+  if (suzuanne) {
+    suzuanne.rotation.x = -elapsedTime * 0.1;
+    suzuanne.rotation.y = elapsedTime * 0.2;
+  }
+
+  sphere.rotation.x = -elapsedTime * 0.1;
+  sphere.rotation.y = elapsedTime * 0.2;
+
+  torusKnot.rotation.x = -elapsedTime * 0.1;
+  torusKnot.rotation.y = elapsedTime * 0.2;
+
+  // update controls
+  controls.update();
+
+  // render
+  renderer.render(scene, camera);
+
+  // call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+tick();
---
+++ shaders/holographic/fragment.glsl
@@ -0,0 +1,9 @@
+varying vec3 vPosition;
+
+
+
+ void main(){
+    gl_FragColor = vec4(vPosition 1.0); // green color
+    #include <tonemapping_fragment>
+    #include <colorSpace_fragment>
+ }
