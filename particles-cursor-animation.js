6d8a3cfa91cc82c3c1af8431390e50476e194851
emmz3230 <emmz3230@gmail.com>
2025-09-04 23:15:14 +0100

    particles cursor done and particle morphing started

--- particles-cursor-animation.js
+++ particles-cursor-animation.js
@@ -1,18 +1,11 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
-import GUI from "lil-gui";
-import gsap from "gsap";
 import particlesVertexShader from "./shaders/particles/vertex.glsl";
 import particlesFragmentShader from "./shaders/particles/fragment.glsl";
 
 /**
  * Base
  */
-// Debug
-const gui = new GUI({ width: 340 });
-const debugObject = {};
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -20,11 +13,7 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
-// Loaders
-const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath("./draco/");
-const gltfLoader = new GLTFLoader();
-gltfLoader.setDRACOLoader(dracoLoader);
+const textureLoader = new THREE.TextureLoader();
 
 /**
  * Sizes
@@ -42,7 +31,7 @@ window.addEventListener("resize", () => {
   sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
   // Materials
-  particles.material.uniforms.uResolution.value.set(
+  particlesMaterial.uniforms.uResolution.value.set(
     sizes.width * sizes.pixelRatio,
     sizes.height * sizes.pixelRatio
   );
@@ -66,7 +55,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.set(0, 0, 8 * 2);
+camera.position.set(0, 0, 18);
 scene.add(camera);
 
 // Controls
@@ -81,44 +70,124 @@ const renderer = new THREE.WebGLRenderer({
   antialias: true,
 });
 
+renderer.setClearColor("#181818");
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(sizes.pixelRatio);
 
-debugObject.clearColor = "#160920";
-gui.addColor(debugObject, "clearColor").onChange(() => {
-  renderer.setClearColor(debugObject.clearColor);
+// displacement
+const displacement = {};
+displacement.canvas = document.createElement("canvas");
+displacement.canvas.width = 128;
+displacement.canvas.height = 128;
+displacement.canvas.style.position = "fixed";
+displacement.canvas.style.width = "256px";
+displacement.canvas.style.height = "256px";
+displacement.canvas.style.top = 0;
+displacement.canvas.style.left = 0;
+displacement.canvas.style.zIndex = 10;
+document.body.append(displacement.canvas);
+
+// context
+displacement.context = displacement.canvas.getContext("2d");
+displacement.context.fillRect(
+  0,
+  0,
+  displacement.canvas.width,
+  displacement.canvas.height
+);
+
+// glow image
+displacement.glowImage = new Image();
+displacement.glowImage.src = "./static/glow.png";
+
+// interactive plane
+displacement.interactivePlane = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshBasicMaterial({
+    color: "red",
+    side: THREE.DoubleSide,
+  })
+);
+displacement.interactivePlane.visible = false;
+scene.add(displacement.interactivePlane);
+
+// raycaster
+displacement.raycaster = new THREE.Raycaster();
+
+// cooordinate
+displacement.screenCursor = new THREE.Vector2(9999, 9999);
+displacement.canvasCursor = new THREE.Vector2(9999, 9999);
+displacement.canvasCursorPrevious = new THREE.Vector2(9999, 9999);
+
+window.addEventListener("pointermove", (event) => {
+  displacement.screenCursor.x = (event.clientX / sizes.width) * 2 - 1;
+  displacement.screenCursor.y =
+    -(event.clientY / sizes.height) * 2 + 1;
 });
-renderer.setClearColor(debugObject.clearColor);
+
+// texture
+displacement.texture = new THREE.CanvasTexture(displacement.canvas);
 
 /**
  * Particles
  */
-const particles = {};
 
 // Geometry
-particles.geometry = new THREE.SphereGeometry(3);
+const particlesGeometry = new THREE.PlaneGeometry(10, 10, 128, 128);
+particlesGeometry.setIndex(null);
+particlesGeometry.deleteAttribute("normal");
+
+const intensitiesArray = new Float32Array(
+  particlesGeometry.attributes.position.count
+);
+
+const anglesArray = new Float32Array(
+  particlesGeometry.attributes.position.count
+);
+
+for (
+  let i = 0;
+  i < particlesGeometry.attributes.position.count;
+  i++
+) {
+  intensitiesArray[i] = Math.random();
+  anglesArray[i] = Math.random() * Math.PI * 2;
+}
+
+particlesGeometry.setAttribute(
+  "aIntensity",
+  new THREE.BufferAttribute(intensitiesArray, 1)
+);
+particlesGeometry.setAttribute(
+  "aAngle",
+  new THREE.BufferAttribute(anglesArray, 1)
+);
 
 // Material
-particles.material = new THREE.ShaderMaterial({
+const particlesMaterial = new THREE.ShaderMaterial({
   vertexShader: particlesVertexShader,
   fragmentShader: particlesFragmentShader,
   uniforms: {
-    uSize: new THREE.Uniform(0.4),
     uResolution: new THREE.Uniform(
       new THREE.Vector2(
         sizes.width * sizes.pixelRatio,
         sizes.height * sizes.pixelRatio
       )
     ),
+    uPictureTexture: new THREE.Uniform(
+      textureLoader.load("./static/picture-1.png")
+    ),
+    uDisplacementTexture: new THREE.Uniform(displacement.texture),
   },
+  blending: THREE.AdditiveBlending,
 });
 
-// Points
-particles.points = new THREE.Points(
-  particles.geometry,
-  particles.material
+const particles = new THREE.Points(
+  particlesGeometry,
+  particlesMaterial
 );
-scene.add(particles.points);
+
+scene.add(particles);
 
 /**
  * Animate
@@ -126,6 +195,54 @@ scene.add(particles.points);
 const tick = () => {
   // Update controls
   controls.update();
+
+  // raycaster
+  displacement.raycaster.setFromCamera(
+    displacement.screenCursor,
+    camera
+  );
+  const intersections = displacement.raycaster.intersectObject(
+    displacement.interactivePlane
+  );
+
+  if (intersections.length) {
+    const uv = intersections[0].uv;
+
+    displacement.canvasCursor.x = uv.x * displacement.canvas.width;
+    displacement.canvasCursor.y =
+      (1 - uv.y) * displacement.canvas.height;
+  }
+
+  // displacement
+  displacement.context.globalCompositeOperation = "source-over";
+  displacement.context.globalAlpha = 0.02;
+  displacement.context.fillRect(
+    0,
+    0,
+    displacement.canvas.width,
+    displacement.canvas.height
+  );
+
+  // speed alpha
+  const cursorDistance = displacement.canvasCursorPrevious.distanceTo(
+    displacement.canvasCursor
+  );
+  displacement.canvasCursorPrevious.copy(displacement.canvasCursor);
+  const alpha = Math.min(cursorDistance * 0.05, 1);
+
+  // draw glow
+  const glowSize = displacement.canvas.width * 0.25;
+  displacement.context.globalCompositeOperation = "lighten";
+  displacement.context.globalAlpha = alpha;
+  displacement.context.drawImage(
+    displacement.glowImage,
+    displacement.canvasCursor.x - glowSize * 0.5,
+    displacement.canvasCursor.y - glowSize * 0.5,
+    glowSize,
+    glowSize
+  );
+  // texture
+  displacement.texture.needsUpdate = true;
 
   // Render normal scene
   renderer.render(scene, camera);
---
+++ shaders/particles-morphing/fragment.glsl
@@ -0,0 +1,11 @@
+void main()
+{
+
+    vec2 uv = gl_PointCoord;
+    float distanceTocenter = length(uv - 0.5);
+    float alpha = 0.05 / distanceTocenter -0.1;
+
+    gl_FragColor = vec4(alpha,alpha,alpha,1.0);
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
+}
