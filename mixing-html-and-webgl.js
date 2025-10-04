b3bdb02473b340db4014ff9df22dc07a0f6e75c3
emmz3230 <emmz3230@gmail.com>
2025-10-04 17:19:14 +0100

    importing and optimizing scene

--- mixing-html-and-webgl.js
+++ mixing-html-and-webgl.js
@@ -6,6 +6,7 @@ import { gsap } from "gsap";
 /**
  * Loaders
  */
+let sceneReady = false;
 const loadingBarElement = document.querySelector(".loading-bar");
 const loadingManager = new THREE.LoadingManager(
   // Loaded
@@ -23,6 +24,10 @@ const loadingManager = new THREE.LoadingManager(
       loadingBarElement.classList.add("ended");
       loadingBarElement.style.transform = "";
     }, 500);
+
+    window.setTimeout(() => {
+      sceneReady = true;
+    }, 2000);
   },
 
   // Progress
@@ -125,6 +130,22 @@ gltfLoader.load(
     updateAllMaterials();
   }
 );
+// points of interest
+const raycaster = new THREE.Raycaster();
+const points = [
+  {
+    position: new THREE.Vector3(1.55, 0.3, -0.6),
+    element: document.querySelector(".point-0"),
+  },
+  {
+    position: new THREE.Vector3(0.5, 0.8, -1.6),
+    element: document.querySelector(".point-1"),
+  },
+  {
+    position: new THREE.Vector3(1.6, 1.3, -0.7),
+    element: document.querySelector(".point-2"),
+  },
+];
 
 /**
  * Lights
@@ -196,6 +217,42 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 const tick = () => {
   // Update controls
   controls.update();
+
+  if (sceneReady) {
+    // go through each point
+    for (const point of points) {
+      const screenPosition = point.position.clone();
+      screenPosition.project(camera);
+
+      raycaster.setFromCamera(screenPosition, camera);
+      const intersects = raycaster.intersectObjects(
+        scene.children,
+        true
+      );
+
+      if (intersects.length == 0) {
+        point.element.classList.add("visible");
+      } else {
+        const intersectionDistance = intersects[0].distance;
+        const pointDistance = point.position.distanceTo(
+          camera.position
+        );
+
+        if (intersectionDistance < pointDistance) {
+          point.element.classList.remove("visible");
+        } else {
+          point.element.classList.add("visible");
+        }
+
+        // point.element.classList.remove("visible");
+      }
+
+      const translateX = screenPosition.x * sizes.width;
+      const translateY = -screenPosition.y * sizes.width;
+
+      point.element.style.transform = `translate(${translateX}px, ${translateY}px)`;
+    }
+  }
 
   // Render
   renderer.render(scene, camera);
